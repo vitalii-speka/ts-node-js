f9ff4ba23c04449068303cf1a4b1282ef57b309d
vitalii-speka <65010140+vitalii-speka@users.noreply.github.com>
2024-10-30 15:41:00 +0100

    created a simple app node.js typescript

---
+++ src/app/domain/person/Person.ts
@@ -0,0 +1,71 @@
+import {
+  JsonController,
+  Get,
+  Post,
+  Body,
+  Param,
+  UseAfter,
+} from "routing-controllers";
+import { validate } from "class-validator";
+
+import { IPerson } from "./Person.types";
+import { CreatePerson } from "./CreatePerson.dto";
+
+import { ApiResponse } from "helpers/ApiResponse";
+import { ApiError } from "helpers/ApiError";
+import { HTTPResponseLogger } from "app/middlewares/HTTPResponseLogger";
+
+const storeData: IPerson[] = [];
+
+@JsonController("/person")
+export default class Person {
+  @Get()
+  @UseAfter(HTTPResponseLogger)
+  async getAll() {
+    return new ApiResponse(true, storeData);
+  }
+
+  @Get("/:id")
+  async getOne(@Param("id") id: number): Promise<ApiResponse<IPerson | {}>> {
+    const person = storeData.find((item) => {
+      return item.id === id;
+    });
+
+    if (!person) {
+      throw new ApiError(404, {
+        code: "PERSON_NOT_FOUND",
+        message: `Person with id ${id} not found`,
+      });
+    }
+
+    return new ApiResponse(true, person);
+  }
+
+  /*  old   @Post() code
+  @Post()
+  async setPerson(@Body() body: IPerson) {
+    storeData.push(body);
+
+    return new ApiResponse(true, "Person successfully created");
+  }
+  */
+  @Post()
+  async setPerson(@Body() body: CreatePerson) {
+    // validate the body using class-validator
+    const errors = await validate(body);
+
+    if (errors.length > 0) {
+      throw new ApiError(400, {
+        message: "Validation failed",
+        code: "PERSON_VALIDATION_ERROR",
+        errors,
+      });
+    }
+
+    const id = storeData.length;
+
+    storeData.push({ ...body, id });
+
+    return new ApiResponse(true, "Person successfully created");
+  }
+}
